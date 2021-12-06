dc86316483afce390a72bd0e2145d40bf25cef7a
a.ashihmin <a.ashihmin@itransition.com>
2021-12-06 16:58:37 +0300

    connect mysql db, enable typeOrm, fix elints and tsconfigs

--- packages/backend/src/app.ts
+++ packages/backend/src/app.ts
@@ -1,19 +1,56 @@
+import 'reflect-metadata';
+
 import bodyParser from 'body-parser';
 import dotenv from 'dotenv';
 import express, { Express, Request, Response } from 'express';
 import helmet from 'helmet';
+import mysql from 'mysql';
+import { createConnection } from 'typeorm';
+import { MysqlConnectionOptions } from 'typeorm/driver/mysql/MysqlConnectionOptions';
+
+import { mainConfig } from './config';
+import { User } from './db/models';
 
 dotenv.config();
 
 const PORT = process.env.PORT || 3030;
 const app: Express = express();
 
-app.use(helmet());
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-
-app.get('/', (_req: Request, res: Response) => {
-  res.send(`<h1>Hello from the server side!</h1>`);
+const dbConnection = mysql.createConnection({
+  host: process.env.DATABASE_HOST,
+  port: parseInt(process.env.DATABASE_PORT!, 10),
+  user: process.env.DATABASE_USER,
+  password: process.env.DATABASE_PASSWORD,
+  database: process.env.DATABASE_NAME,
 });
 
-app.listen(PORT, () => console.log(`Running on ${PORT} ⚡`));
+dbConnection.connect((err: Error) => {
+  if (err) {
+    console.log('ERROR');
+    throw err;
+  }
+
+  console.log('Connected!');
+});
+console.log(mainConfig.database);
+createConnection(mainConfig.database as MysqlConnectionOptions).then(async connection => {
+  const repository = connection.getRepository(User);
+  const user = new User();
+  user.firstName = 'Admin';
+  user.lastName = 'Adminuser';
+
+  await repository.save(user);
+
+  app.use(helmet());
+  app.use(bodyParser.json());
+  app.use(bodyParser.urlencoded({ extended: true }));
+
+  app.get('/', (_req: Request, res: Response) => {
+    res.send(`
+    <h1>Hello from the server side!</h1>
+    <h3>DB connection: ${dbConnection.state}</h3> 
+  `);
+  });
+
+  app.listen(PORT, () => console.log(`Running on ${PORT} ⚡`));
+});
---
+++ packages/backend/src/config/main.ts
@@ -0,0 +1,17 @@
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+export const mainConfig = {
+  database: {
+    type: 'mysql',
+    host: process.env.DATABASE_HOST,
+    port: parseInt(`${process.env.DATABASE_PORT}`, 10),
+    username: process.env.DATABASE_USER,
+    password: process.env.DATABASE_PASSWORD,
+    database: process.env.DATABASE_NAME,
+    synchronize: true,
+    logging: true,
+    entities: [`${__dirname}/../**/*.model.{ts,js}`],
+  },
+};
